801073f25a087c9101764b76ce62289ef5ab6c3b
Huy Dang Tuan <huy.dang@orientsoftware.com>
2019-05-20 13:41:34 +0700

    s

--- src/store.js
+++ src/store.js
@@ -10,6 +10,7 @@ export default new Vuex.Store({
       { invId: 2, name: 'Vue T-shirt', image: 'https://i.imgur.com/TriMZO9.jpg', price: 999 },
       { invId: 3, name: 'React T-shirt', image: 'https://i.imgur.com/Bh7hynu.jpg', price: 966 },
       { invId: 4, name: 'Aurelia T-shirt', image: 'https://i.imgur.com/PJt6yv8.jpg', price: 299 },
+      { invId: 5, name: 'Kara', image: 'https://i.imgur.com/OCNAMa1.jpg', price: 999 },
     ],
     inCart: [],
   },
